58f9922f7a5ec8ca85c8fd4e52d57e5d3b279b7f
EL JUGGERNAUT <43868101+jungiejungle@users.noreply.github.com>
2018-10-08 10:59:33 +0800

    Update and rename .gitignore to hello.py

---
+++ .gitignore/hello.py
@@ -0,0 +1,19 @@
+print("Hello, World")
+print("Python", 3)
+print("Hello, World", sep=";", end="$")
+print("{}{}".format("ADNU", 2018))
+
+
+age = 18
+weight = 60.21
+
+print (type(age))
+print (type(weight))
+print (type("Hello"))
+
+data = {"name" : "Juggernaut",
+        "age" : 18,
+        "height" : 178.50,
+        "fave_fruits" : ["apple" , "mango" , "batag"]
+}
+print("My name is {}".format.(data["name"]))
